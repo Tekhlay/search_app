5abc62c480edfa8ab9b2d30a20318343020e57ed
Tekhlay <teklaybirhane12@yahoo.com>
2024-05-22 23:29:13 +0300

    Update search page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,16 @@
 class SearchController < ApplicationController
   def index
     @search = SearchQuery.all
-    render json: @search
+    # render json: @search
   end
 
   def create
-    @search = SearchQuery.new(search_params)
+    @query = params[:query]
+    @search = SearchQuery.create(query: @query, ip_address: request.remote_ip)
 
-    if @search.save
-      redirect_to @search
-    else
-      render 'new'
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.json { render json: @search }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'search/index'
+  resources :search, only: [:index, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "search#index"
 end
