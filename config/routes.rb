7aa819d64baf9c1cda17dc2d99d72c6a035dd379
Tekhlay <teklaybirhane12@yahoo.com>
2024-05-23 10:54:48 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'analytics/index'
-  get 'search/index'
-  post 'search/create', to: 'search#create', as: 'search_create'
-  resources :search, only: [:index, :create]
+  post 'search/log', to: 'search#log'
+  # post 'search/create', to: 'search#create', as: 'search_create'
+  resources :search, only: [:index, :log]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
