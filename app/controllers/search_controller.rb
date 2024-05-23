a1695b64a169abefc01a96d56916e36042243c3e
Tekhlay <teklaybirhane12@yahoo.com>
2024-05-23 10:54:14 +0300

    Change create to log

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,18 @@
 class SearchController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:log]
   def index
     @search_queries = SearchQuery.all
   end
 
-  def create
-    @query = params[:query]
-    @search_query = SearchQuery.create(query: @query, ip_address: request.remote_ip)
+  def log
+    query = params[:query].strip
+    ip_address = request.remote_ip
 
-    respond_to do |format|
-      format.js { render partial: 'search/search_results', locals: { query: @query } }
-      format.html { redirect_to root_path }
-      format.json { render json: @search_query }
+    unless query.empty?
+      SearchQuery.create(query: query, ip_address: ip_address)
+      render json: { success: true }
+    else
+      render json: { success: false, error: 'Empty query' }, status: :unprocessable_entity
     end
   end
 end
