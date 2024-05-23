6116631fc30423fdbbadced183174795753657ff
Tekhlay <teklaybirhane12@yahoo.com>
2024-05-23 07:23:53 +0300

    update search view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,16 @@
 class SearchController < ApplicationController
   def index
-    @search = SearchQuery.all
-    # render json: @search
+    @search_queries = SearchQuery.all
   end
 
   def create
     @query = params[:query]
-    @search = SearchQuery.create(query: @query, ip_address: request.remote_ip)
+    @search_query = SearchQuery.create(query: @query, ip_address: request.remote_ip)
 
     respond_to do |format|
+      format.js { render partial: 'search/search_results', locals: { query: @query } }
       format.html { redirect_to root_path }
-      format.json { render json: @search }
+      format.json { render json: @search_query }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'analytics/index'
   get 'search/index'
+  post 'search/create', to: 'search#create', as: 'search_create'
   resources :search, only: [:index, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "search#index"
+  root 'search#index'
 end
