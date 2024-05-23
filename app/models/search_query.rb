74580a9f97029b3d1fc2734a8ada8c954c1d6701
Tekhlay <teklaybirhane12@yahoo.com>
2024-05-23 06:14:18 +0300

    Add validations

--- app/models/search_query.rb
+++ app/models/search_query.rb
@@ -1,2 +1,8 @@
 class SearchQuery < ApplicationRecord
+    validates :query, presence: true
+    validates :ip_address, presence: true
+
+    scope :unique_queries, -> { select('DISTINCT query').count }
+    scope :top_queries, -> (limit = 10) { group(:query).order('count_id DESC').limit(limit).count('id') }
+    scope :by_ip_address, -> (ip_address) { where(ip_address: ip_address) }
 end
