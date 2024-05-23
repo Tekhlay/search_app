fe1ded5d64ef1555c267a7ecad42ba0496e75ce3
Tekhlay <teklaybirhane12@yahoo.com>
2024-05-23 21:47:08 +0300

    Ad more test cases

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe SearchController, type: :controller do
-    describe 'POST #create' do
-        it 'creates a new search query' do
-            post :log, params: { query: 'what is' }
-            expect(SearchQuery.count).to eq(0)
-        end
+  describe 'POST #log' do
+    it 'does not create a new search query for an incomplete query' do
+      post :log, params: { query: 'what is' }
+      expect(SearchQuery.count).to eq(0)
     end
+
+    it 'creates a new search query for a complete query' do
+      post :log, params: { query: 'What is a good car' }
+      expect(SearchQuery.count).to eq(1)
+    end
+
+    it 'does not create a new search query for an empty query' do
+      post :log, params: { query: '' }
+      expect(SearchQuery.count).to eq(0)
+    end
+
+    it 'returns success for a complete query' do
+      post :log, params: { query: 'How is emil hajric doing' }
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns error for an incomplete query' do
+      post :log, params: { query: 'How is' }
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
 end
