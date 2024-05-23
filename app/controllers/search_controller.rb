dab56f3888456737c5e53c681e64046b0c4cc975
Tekhlay <teklaybirhane12@yahoo.com>
2024-05-23 21:47:30 +0300

    Update algorithm

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:log]
+
   def index
     @search_queries = SearchQuery.all
   end
@@ -8,11 +9,30 @@ class SearchController < ApplicationController
     query = params[:query].strip
     ip_address = request.remote_ip
 
-    unless query.empty?
+    if meaningful_query?(query)
       SearchQuery.create(query: query, ip_address: ip_address)
       render json: { success: true }
     else
-      render json: { success: false, error: 'Empty query' }, status: :unprocessable_entity
+      render json: { success: false, error: 'Incomplete or meaningless query' }, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def meaningful_query?(query)
+    return false if query.blank?
+
+    stop_words = %w(a an the and or)
+    words = query.split
+    known_verbs = %w(is are was were do does has have had be being been)
+    filtered_words = words - stop_words
+    has_multiple_words = filtered_words.size > 1
+
+    has_verbs = filtered_words.any? { |word| known_verbs.include?(word.downcase) }
+
+    has_nouns = filtered_words.any? { |word| word.match(/\w+/) && !known_verbs.include?(word.downcase) }
+
+    
+    has_multiple_words && has_verbs && has_nouns
   end
 end
